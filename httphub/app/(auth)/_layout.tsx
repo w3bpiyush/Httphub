52724b68ae2635789c880d07bebe4c5dcc98ef25
Piyush Manna <piyushmanna69@gmail.com>
2025-08-23 15:44:44 +0530

    deleted

--- httphub/app/(auth)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack, Redirect } from "expo-router";
-import { useAuth } from "../../context/AuthContext";
-import React from "react";
-
-export default function AuthLayout() {
-  const { isAuthenticated } = useAuth();
-  if (isAuthenticated) return <Redirect href="/(home)" />;
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
